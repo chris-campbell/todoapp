952cd166ce2f8d785b391aa4d8443e9774310e4a
Chris Camp <christopher.campbell@outlook.com>
2020-11-17 19:25:44 -0500

    Setup application layout
    - Create navigation layout
    - Create AddFragment
    - Create List Fragment with Recyclerview
    - Create update fragment for list items
    - Create menu for list fragment
    - Create menu for add fragment
    - Create menu for update fragment

---
+++ app/src/main/java/tech/hoppr/todoapp/fragments/list/ListFragment.kt
@@ -0,0 +1,37 @@
+package tech.hoppr.todoapp.fragments.list
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_list.view.*
+import tech.hoppr.todoapp.R
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        view.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        setHasOptionsMenu(true)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+
+}
